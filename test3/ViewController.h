bc7c4f6f586937dfcd8b7cc9776f6c58fff5f5c4
monadis <firesign@naver.com>
2014-08-16 21:05:06 +0900

    Initial Commit

---
+++ test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test3
+//
+//  Created by LYH on 2014. 8. 16..
+//  Copyright (c) 2014년 YK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test3
+//
+//  Created by LYH on 2014. 8. 16..
+//  Copyright (c) 2014년 YK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
